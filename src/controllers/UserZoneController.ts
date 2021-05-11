22c04bb6a4a79808cbcc25d8ee1310671ed066d9
Damiao <tower_gamer@hotmail.com>
2021-05-11 20:18:20 -0300

    Chore: Verification provider exists in area

--- src/controllers/UserZoneController.ts
+++ src/controllers/UserZoneController.ts
@@ -14,6 +14,14 @@ class UserZoneController {
         const { user_id } = request.body;
         const userZonesRepository = getCustomRepository(UserZoneRepository);
 
+        const userZoneExists = await userZonesRepository.findOne({ user_id, zone_id: zoneId });
+
+        if (userZoneExists) {
+            return response.status(400).json({ 
+                error: "Já existe um fornecedor nessa área." 
+            })
+        }
+
         const user_zone = userZonesRepository.create({
             zone_id: zoneId,
             user_id,

c3157abdfb86d95729eef4ea9fbcf7948afc47c8
Damiao <tower_gamer@hotmail.com>
2021-05-21 14:07:07 -0300

    Chore: remove repositories

--- src/repositories/CityRepository.ts
+++
@@ -1,7 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import { City } from "../models/City";
-
-@EntityRepository(City)
-class CityRepository extends Repository<City> {}
-
-export { CityRepository };

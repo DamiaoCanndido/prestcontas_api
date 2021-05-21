62e8b4455bf8ece29c54f71c9c7a5d26769de83b
Damiao <tower_gamer@hotmail.com>
2021-05-21 14:50:05 -0300

    Chore: ormconfig conditional

--- ormconfig.js
+++ ormconfig.js
@@ -2,9 +2,12 @@ module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
     "logging": true,
-    // "synchronize": true,
-    "migrations": [process.env.MIGRATIONS_PROD],
-    "entities": [process.env.ENTITIES_PROD],
+    "migrations": process.env.NODE_ENV === "development" 
+        ? ["./src/database/migrations/**.ts"]
+        : ["./dist/database/migrations/**.js"],
+    "entities": process.env.NODE_ENV === "development" 
+        ? ["./src/models/**.ts"]
+        : ["./dist/models/**.js"],
     "cli": {
         "migrationsDir": "./src/database/migrations",
         "entitiesDir": "./src/models"
